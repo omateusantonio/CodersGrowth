6a380cda77169cacc58818e66207737d1041dc2a
MateusAntonio <mateusantonio23@live.com>
2023-11-17 13:05:26 -0300

    implementacao dos codigos e criacao da tabela dentro do banco de dados

---
+++ src/projetocodersgrowth/CodersGrowthProjeto.Dominio/CriaTabelaAnimaisSilvestres.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodersGrowthProjeto.Dominio;
+using FluentMigrator;
+
+namespace ControleDeAnimaisSilvestres.Dominio
+{
+    [Migration(20231117111300)] //cada migração é pra uma determinada atualização do banco de dados?
+    public class CriaTabelaAnimaisSilvestres : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Controle de Animais Silvestres Resgatados")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Nome do Animal").AsString()
+                .WithColumn("Nome da Especie").AsString()
+                .WithColumn("Classe de Animal").AsString()
+                .WithColumn("Data do Resgate").AsDate()
+                .WithColumn("Esta em Extincao").AsBoolean()
+                .WithColumn("Custo da Vacinacao").AsCurrency();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Controle de Animais Silvestres Resgatados");
+        }
+    }
+}
