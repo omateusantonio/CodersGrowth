abe1636dc9bb14ffdf66d6a604d7cc5558b1e336
MateusAntonio <mateusantonio23@live.com>
2023-11-20 12:46:06 -0300

    criacao do repositorio com as operacoes de crud e da variavel de conexao com o banco de dados

---
+++ src/projetocodersgrowth/CodersGrowthProjeto.Dominio/ConexaoSql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAnimaisSilvestres.Dominio
+{
+    internal class Class1
+    {
+    }
+}
---
+++ src/projetocodersgrowth/CodersGrowthProjeto.Dominio/RepositorioSql.cs
@@ -0,0 +1,44 @@
+﻿using CodersGrowthProjeto.Dominio;
+using System;
+using System.Configuration;
+using System.Configuration.Assemblies;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ControleDeAnimaisSilvestres.Dominio
+{
+    
+    public class RepositorioSql : IRepositorio
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["BancoDeDados"].ConnectionString;
+        private static SqlConnection sqlConn = new SqlConnection(connectionString);
+        public List<AnimalSilvestre> ObterTodos()
+        {
+            sqlConn.Open();
+        }
+
+        public void Criar(AnimalSilvestre animalNovo)
+        {
+
+        }
+
+        public void Remover(int id)
+        {
+
+        }
+
+        public AnimalSilvestre ObterPorId(int id)
+        {
+
+        }
+
+        public void Atualizar(AnimalSilvestre animalSelecionado)
+        {
+
+        }
+    }
+}
