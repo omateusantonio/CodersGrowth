accfe36380d5f6d3808f1d285f112815e5b461cc
MateusAntonio <mateusantonio23@live.com>
2023-12-11 12:53:55 -0300

    alteracao na appsettings

---
+++ src/projetocodersgrowth/ControleDeAnimaisSilvestres.WebApp/controller/CRUDController.cs
@@ -0,0 +1,36 @@
+﻿using ControleDeAnimaisSilvestres.Dominio.Objetos;
+using ControleDeAnimaisSilvestres.Infra.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeAnimaisSilvestres.WebApp.controller
+{
+    [Produces("application/json")]
+    [Route("api/animais")]
+
+    public class CRUDController : ControllerBase
+    {
+        private readonly RepositorioSql servico;
+
+        [Route("~/api/ObterTodosAnimais")]
+        [HttpGet]
+        public ActionResult<List<AnimalSilvestre>> ObterTodos()
+        {
+                var modelo = servico.ObterTodos();
+
+                return Ok(modelo);
+
+        }
+
+        [HttpGet("{id}", Name = "ObterPorId")]
+        public ActionResult ObterPorId(int id)
+        {
+            var modelo = servico.ObterPorId(id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+            return Ok(modelo);
+        }
+
+    }
+}
