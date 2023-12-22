945bb6d68b48ce8f2aaf02f0fd8b958a3904fa86
MateusAntonio <mateusantonio23@live.com>
2023-12-22 14:01:45 -0300

    inicio da criacao do metodo para fazer o fetch com post

--- src/projetocodersgrowth/ControleDeAnimaisSilvestres.WebApp/wwwroot/controller/Cadastro.controller.js
+++ src/projetocodersgrowth/ControleDeAnimaisSilvestres.WebApp/wwwroot/controller/Cadastro.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
-    "sap/ui/model/json/JSONModel"
-], (Controller, History, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/format/DateFormat"
+], (Controller, History, JSONModel, DateFormat) => {
     "use strict";
 
     return Controller.extend("ui5.controledeanimaissilvestres.controller.Cadastro", {
@@ -13,11 +14,7 @@ sap.ui.define([
         },
 
         aoCoincidirRota() {
-            const sheetNames = {listaDeClasses : [{ Classe : "Anfíbio"}, {Classe : "Ave"}, {Classe : "Mamífero"}, {Classe : "Peixe"}, {Classe : "Réptil"}] };
-            const sheets = new JSONModel(sheetNames);
-            const comboBox = this.getView().byId("inputClasseDeAnimal");
-
-            comboBox.setModel(sheets);
+            this.definirItensDaCombobox();
         },
 
         aoClicarEmVoltar() {
@@ -32,9 +29,76 @@ sap.ui.define([
             }
         },
 
-        aoClicarEmCancelarCadastro() {
+        aoClicarEmCancelar() {
             const oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("lista", {}, true);
+        },
+
+        aoClicarEmSalvar() {
+            const oView = this.getView();
+            const nomeDoAnimal = oView.byId("inputNomeDoAnimal").getValue();
+            const nomeDaEspecie = oView.byId("inputNomeDaEspecie").getValue();
+            const classeDeAnimal = oView.byId("inputClasseDeAnimal").getValue();
+            const precoDaVacinacao = oView.byId("inputPrecoDaVacinacao").getValue();
+            // const dataDoResgate = oView.byId("inputDataDoResgate").getValue();
+            const dataDoResgate = this.converterDataParaFormatoDeBanco();
+            const emExtincao = oView.byId("inputEmExtincao").getProperty("selected");
+
+            console.log(nomeDoAnimal);
+            console.log(nomeDaEspecie);
+            console.log(classeDeAnimal);
+            console.log(precoDaVacinacao);
+            console.log(dataDoResgate);
+            console.log(emExtincao);
+
+            const sItems = [{nomeDoAnimal : nomeDoAnimal, 
+                            nomeDaEspecie : nomeDaEspecie,
+                            dataDoResgate : dataDoResgate,
+                            classe : classeDeAnimal,
+                            emExtincao : emExtincao,
+                            custoDeVacinacao : precoDaVacinacao}]
+
+            const oModelJson = new JSONModel(sItems);
+            const oDados = oModelJson.getData();
+
+            console.log(oDados);
+
+            this.cadastrarNovoAnimal(oDados);
+        },
+
+        definirItensDaCombobox() {
+            const sheetNames = {listaDeClasses : [{ Classe : "Anfíbio"}, {Classe : "Ave"}, {Classe : "Mamífero"}, {Classe : "Peixe"}, {Classe : "Réptil"}] };
+            const sheets = new JSONModel(sheetNames);
+            const comboBox = this.getView().byId("inputClasseDeAnimal");
+
+            comboBox.setModel(sheets);
+        },
+
+        converterDataParaFormatoDeBanco() {
+            const oView = this.getView();
+            const dataDoResgate = oView.byId("inputDataDoResgate").getValue();
+            
+            if (dataDoResgate) {
+                var oDateFormat = DateFormat.getDateInstance({
+                    pattern: "yyyy-MM-ddTHH:mm:ss"
+                });
+
+                var oDate = new Date(dataDoResgate);
+
+                return oDateFormat.format(oDate);
+            }
+            
+        },
+
+        cadastrarNovoAnimal(dados) {
+            fetch('/api/AnimalSilvestre', {
+                method: "POST",
+                body: JSON.stringify(dados),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(erro => console.error(erro));
         }
     });
 });
